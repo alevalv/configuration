8c83af9587e1a95f8619e7f3fedaee1de5a381c6
Alejandro Valdes <alejandrovaldes@live.com>
2019-03-20 22:58:25 -0500

    Added script to link existing files

---
+++ dotfiles/README.md
@@ -0,0 +1,5 @@
+# Dotfiles configuration folder
+
+This folder contains the dotfiles for the home and root folder, the `link.py` command will currently link all files inside the home folder to the current user directory. This process will set the link over any existing file.
+
+By default is set to only verify existing files in the user directory, modify the `verify` variable to `False` to set all links.
---
+++ dotfiles/link.py
@@ -0,0 +1,55 @@
+#! /bin/env python3
+
+import glob
+import os
+from pathlib import Path
+
+#arguments
+dir = 'home/'
+verify = True
+ignore_files = ['onedrive']
+
+def ignore(path: str):
+    for ignore in ignore_files:
+        if path.find(ignore) is not -1:
+            return True
+    return False
+
+
+repo_directory = os.path.dirname(os.path.abspath(__file__))
+print("Configuration repo in "+ repo_directory)
+files_to_link = []
+
+for filename in glob.iglob(dir + '**', recursive=True):
+    if Path(filename).is_file():
+        files_to_link.append(filename[len(dir):])
+
+print("Found {} files to link".format(len(files_to_link)))
+
+print("Switching directory to ~")
+
+os.chdir(os.path.expanduser('~'))
+
+for file in files_to_link:
+    target_link = "." + file
+    repo_file = repo_directory + "/" + dir + file
+    if verify:
+        file = Path(target_link)
+        if not file.is_symlink() and file.exists():
+            print("Target link {} exists, this file will be removed if linked".format(target_link))
+        if not file.exists():
+            print("Target link {} can be linked".format(target_link))
+        if file.is_symlink():
+            print("{} is a symlink".format(target_link))
+    elif not ignore(target_link):
+        #check if the symlink directory exists, if not, create it:
+        target_directory = target_link[:target_link.rfind("/")]
+        if not os.path.exists(target_directory):
+            os.makedirs(target_directory)
+        #if the file exists, delete it
+        if os.path.exists(target_link):
+            os.remove(target_link)
+        print("Linking {} -> {}".format(target_link, repo_file))
+        os.symlink(repo_file, target_link)
+    else:
+        print("Ignoring " + target_link)
